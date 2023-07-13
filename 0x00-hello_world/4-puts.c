582652efd112bc4b80ebbd4f3e832448be9ebfd6
Icon Makhesa <makhesa2030@gmail.com>
2023-07-13 05:37:40 -0700

    c text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - A pragram that print using put function
+ * Return: 0( Sucess)
+ * */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
