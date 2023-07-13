5d780d056f0353f9f716e24ef617c527b43acfac
Icon Makhesa <makhesa2030@gmail.com>
2023-07-13 05:43:40 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/**
- * main - A pragram that print using put function
- * Return: 0( Sucess)
- *
- */
+/*
+*main - A pragram that print using put function
+*Return: 0( Sucess)
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
