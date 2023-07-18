a406b828d6e9f86820e6ffb9ff686992682c7b9c
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 14:40:10 -0700

    plk

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
+*/
 int main(void)
 {
-unsigned int fib1 = 1, fib2 = 2, fib3;
-unsigned int count, sum;
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
 
-printf("%u, %u", fib1, fib2);
-
-sum = fib2;
-
-for (count = 3; count <= 100; count++)
+for (inc = 0; inc < 92; inc++)
 {
-fib3 = fib1 + fib2;
-if (fib3 > 4000000)
-break;
-
-printf(", %u", fib3);
-
-if (fib3 % 2 == 0)
-sum += fib3;
-
-fib1 = fib2;
-fib2 = fib3;
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
 }
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
+{
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
 
-printf("\nSum: %u\n", sum);
-
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
+}
+printf("\n");
 return (0);
 }
