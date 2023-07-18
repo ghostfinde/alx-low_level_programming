4532e47b6b2a6476db55282e18647f0a88c3e2c6
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 14:12:32 -0700

    n

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,29 @@
  */
 int main(void)
 {
-unsigned long fib1 = 1, fib2 = 2, fib3;
-int count;
+unsigned int fib1 = 1, fib2 = 2, fib3;
+unsigned int count, sum;
 
-printf("%lu, %lu", fib1, fib2);
+printf("%u, %u", fib1, fib2);
 
-for (count = 3; count < 98; count++)
+sum = fib2;
+
+for (count = 3; count <= 98; count++)
 {
 fib3 = fib1 + fib2;
-printf(", %lu", fib3);
+if (fib3 <= 4000000)
+{
+printf(", %u", fib3);
+
+if (fib3 % 2 == 0)
+sum += fib3;
+}
 
 fib1 = fib2;
 fib2 = fib3;
 }
 
-printf("\n");
+printf("\nSum: %u\n", sum);
+
 return (0);
 }
