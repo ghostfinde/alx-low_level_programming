76874285a882ae0919930ac90a40a263f9d13d81
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 13:00:13 -0700

    k

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long int fib1 = 1, fib2 = 2, fib3;
+int count;
+
+printf("%lu, %lu, ", fib1, fib2);
+
+for (count = 2; count < 98; count++)
+{
+fib3 = fib1 + fib2;
+printf("%lu", fib3);
+
+if (count != 97)
+printf(", ");
+
+fib1 = fib2;
+fib2 = fib3;
+}
+
+printf("\n");
+
+return (0);
+}
