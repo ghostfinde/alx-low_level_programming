cff225cd7cdfd90a3800344b4f579c3e1a440079
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 11:14:34 -0700

    q

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- *print_alphabet_10x - Print the alphabet 10 times.
+ * print_alphabet_10x - Print the alphabet 10 times.
  *
- *Return : Always 0 (Success)
+ * Return : Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
