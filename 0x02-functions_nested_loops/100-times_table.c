c96551ea5f57b57827bfdac839998f848da8e0b2
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 14:36:20 -0700

    ghj

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/** 
-* print_times_table - Prints the n times table, starting with 0 
-* @n: The number of times 
-*/ 
+/**
+* print_times_table - Prints the n times table, starting with 0
+* @n: The number of times
+*/
 void print_times_table(int n)
 {
 int row, col, product;
@@ -10,7 +10,7 @@ if (n < 0 || n > 15)
 return;
 for (row = 0; row <= n; row++)
 {
-for (col = 0; col <= n; col++) 
+for (col = 0; col <= n; col++)
 {
 product = row * col;
 if (col != 0)
