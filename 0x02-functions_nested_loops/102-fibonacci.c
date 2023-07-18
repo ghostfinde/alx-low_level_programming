eec7365285d8ff440bd0d4b63f4ce1ee635ffb1d
Icon Makhesa <makhesa2030@gmail.com>
2023-07-18 12:46:15 -0700

    h

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,16 +9,13 @@ void fibonacci(void)
 int i;
 unsigned long fib1 = 1, fib2 = 2, fib;
 printf("%lu, %lu", fib1, fib2);
-    
 for (i = 3; i <= 50; i++)
 {
 fib = fib1 + fib2;
 printf(", %lu", fib);
-        
 fib1 = fib2;
 fib2 = fib;
 }
-    
 printf("\n");
 }
 int main(void)
