352ecdffba75425f9ca1f016aeb93d766d3d9425
Icon Makhesa <makhesa2030@gmail.com>
2023-07-24 08:18:25 -0700

    g

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character.
+ * puts2 - Prints every other char of a str, starting with 1st char
  * @str: Pointer to the string.
  */
 void puts2(char *str)
@@ -12,12 +12,15 @@ int i = 0;
 while (str[i] != '\0')
 {
 putchar(str[i]);
-i++; /* Increment by 1 to move to the next character*/
+i++;
+/* Increment by 1 to move to the next character*/
 
-if (str[i] == '\0') /* Check if the next character is the null-terminator*/
+if (str[i] == '\0')
+/* Check if the next character is the null-terminator*/
 break;
 
-i++; /*Increment by 1 to skip the next character*/
+i++;
+/*Increment by 1 to skip the next character*/
 }
 putchar('\n');
 }
