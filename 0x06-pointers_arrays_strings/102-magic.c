e909b81fc7ced0f4b9fc0b4ae36cb63c1122e377
Icon Makhesa <makhesa2030@gmail.com>
2023-07-27 13:57:30 -0700

    hhjk

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,9 @@
-/**
-* main - Entry point
-*
-* Return: Always 0
-*/
-
 #include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
@@ -12,7 +11,7 @@ int n;
 int a[5];
 int *p;
 
-a[2] = 98;
+a[2] = 1024;
 p = &n;
 /*
 * write your line of code here...
@@ -22,7 +21,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-p[5] = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
